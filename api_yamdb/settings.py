6f96b3d56830c6b5cd59f59284a28eced96a4377
Игнатов Михаил <Ignatov.mu@yandex.ru>
2021-03-02 00:56:02 +0300

    Change file settings.py

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -14,6 +14,8 @@ import os
 
 from dotenv import load_dotenv
 
+load_dotenv()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -147,5 +149,3 @@ REST_FRAMEWORK = {
         "rest_framework.pagination.PageNumberPagination",
     'PAGE_SIZE': 100,
 }
-
-load_dotenv()
