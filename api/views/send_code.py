6f6760dc8989d9f197eb1eb19a2ffefc367dd0a7
Игнатов Михаил <Ignatov.mu@yandex.ru>
2021-03-02 17:59:06 +0300

    Правка PEP8

--- api/views/send_code.py
+++ api/views/send_code.py
@@ -1,7 +1,6 @@
-from django.core.mail import BadHeaderError, send_mail
 from django.contrib.auth.hashers import make_password
+from django.core.mail import BadHeaderError, send_mail
 from django.shortcuts import get_object_or_404
-from django.utils.crypto import get_random_string
 from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
